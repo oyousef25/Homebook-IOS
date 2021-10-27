6c5b565f6210649f28bd29cba034f918a3427253
Omar Yousef <71517267+oyousef25@users.noreply.github.com>
2021-10-27 18:04:59 -0400

    The default items show successfully on the home page

--- Homebook/ViewControllers/MainViewController.swift
+++ Homebook/ViewControllers/MainViewController.swift
@@ -18,6 +18,8 @@ class MainViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        tableView.dataSource = self
     }
     
     
@@ -29,7 +31,7 @@ class MainViewController: UIViewController {
 extension MainViewController: UITableViewDataSource{
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 5
+        return 1
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
