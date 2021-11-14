b517532e46a49e5f42f92a228352df232dc14f4e
Omar Yousef <71517267+oyousef25@users.noreply.github.com>
2021-11-14 16:36:56 -0500

    Finished adding functionality to the mortgage calculator page

--- Homebook/ViewControllers/MortgageCalculatorViewController.swift
+++ Homebook/ViewControllers/MortgageCalculatorViewController.swift
@@ -29,9 +29,36 @@ class MortgageCalculatorViewController: UIViewController {
     }
     
     //MARK: Calculation methods
+    func calculateMortgage(){
+        let loanAmount = Double(loanAmount.text ?? "") ?? 0.0
+        let interestRate = Double(interestRate.text ?? "") ?? 0.0
+        let loanPeriod = Double(loanPeriod.text ?? "") ?? 0.0
+        let downPayment = Double(downPayment.text ?? "") ?? 0.0
+        
+        //Calculating how many months it will take to pay off the loan
+        let numOfMonths = loanPeriod * 12
+        
+        //Total amount that the loan taker will pay
+        let totalPayment = loanAmount - downPayment
+        
+        //Monthly interest
+        let rate = interestRate / 100 / 12
+        
+        //Monthly payment
+        let result: Double = totalPayment * rate / (1 - pow(1 + rate, -numOfMonths))
+    
+        let formatter = NumberFormatter()
+        formatter.numberStyle = .currency
+        
+        //Using our formatter to formate the monthly payment
+        let formattedResult = formatter.string(from: NSNumber(value: Int(result))) ?? ""
+        
+        
+        calculationResult.text = "\(formattedResult)/Month"
+    }
     
     //MARK: Actions
     @IBAction func calculateButton(_ sender: Any) {
-        calculationResult.text = "$500.00"
+        calculateMortgage()
     }
 }
