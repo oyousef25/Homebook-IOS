9ed4a4560963ef1db41f927e79b8ea8a35f66ca0
Omar Yousef <71517267+oyousef25@users.noreply.github.com>
2021-11-17 14:19:23 -0500

    Finished adding the UI elements on the saved listings page

---
+++ Homebook/TableViewCells/SavedListingTableViewCell.swift
@@ -0,0 +1,20 @@
+//
+//  SavedListingTableViewCell.swift
+//  Homebook
+//
+//  Created by Omar Yousef on 2021-11-17.
+//
+
+import UIKit
+
+class SavedListingTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+    }
+}
--- Homebook/ViewControllers/SavedListingsViewController.swift
+++ Homebook/ViewControllers/SavedListingsViewController.swift
@@ -8,22 +8,35 @@
 import UIKit
 
 class SavedListingsViewController: UIViewController {
+    @IBOutlet weak var tableview: UITableView!
+    /*
+        MARK: Properties
+     */
+    var savedListings = [Listing]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        tableview.dataSource = self
+    }
+}
 
-        // Do any additional setup after loading the view.
+//MARK: TableView DataSource Methods
+extension SavedListingsViewController: UITableViewDataSource{
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
     }
     
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
 
-    /*
-    // MARK: - Navigation
+        //Creating a new instance of our cell
+        let cell = tableView.dequeueReusableCell(withIdentifier: "savedCell", for: indexPath) as! SavedListingTableViewCell
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+        //let listing = savedListings[indexPath.row]
+
+        //Setting up our cell with the values passed by the API
+        //cell.setUpCell(listing: listing)
+
+        //Returning our populated cell
+        return cell
     }
-    */
-
 }
